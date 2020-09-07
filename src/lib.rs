0a221433c2f2dd9c81e583891bc7eb301726ab1f
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2020-09-07 20:04:37 +0900

    access DOM from WebAssembly

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
 use wasm_bindgen::prelude::*;
-use web_sys::console;
 
 #[wasm_bindgen(start)]
-pub fn start() {
-    console::log_1(&"Hello from Rust!".into());
+pub fn main() -> Result<(), JsValue> {
+    let window = web_sys::window().ok_or("no global `window` exists")?;
+    let document = window
+        .document()
+        .ok_or("should have a document on window")?;
+
+    let app = document
+        .get_element_by_id("app")
+        .ok_or("missing `app` in document")?;
+    app.set_inner_html("Hello from Rust!");
+
+    Ok(())
 }
